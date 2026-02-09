22ad07a90d21a3f061aeeb0a1347311900f51995
saniya3012168 <saniyasondkar3012@gmail.com>
2026-02-09 19:57:08 +0530

    Delete profile_routes.py

--- backend/routes/profile_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-profile_bp = Blueprint("profile", __name__)
-
-@profile_bp.route("/", methods=["GET"])
-def profile():
-    return jsonify({"message": "Profile endpoint"})
