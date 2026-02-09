24de78d72795f9abed5d881aae11fcf8081e1f92
saniya3012168 <saniyasondkar3012@gmail.com>
2026-02-09 19:56:17 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -7,6 +7,6 @@ load_dotenv()
 client = MongoClient(os.getenv("MONGO_URI"))
 db = client["jobpilot_db"]
 
-users_collection = db["users"]
-jobs_collection = db["jobs"]
-applications_collection = db["applications"]
+users = db.users
+jobs = db.jobs
+applications = db.applications
