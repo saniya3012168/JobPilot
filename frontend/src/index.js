df968d655eab4168c32bf932203e9bb50523bd80
saniya3012168 <saniyasondkar3012@gmail.com>
2026-02-16 18:41:51 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/main.css';
@@ -11,3 +12,21 @@ root.render(
     <App />
   </React.StrictMode>
 );
+=======
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRoutes from "./routes";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+>>>>>>> main
